730aad9cde0de57d0f37f86602163eaf6d05374f
Arz Dev <arz.dev@outlook.com>
2024-03-17 22:29:26 -0600

    [topics] + classes

---
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,29 @@
+export class Person {
+    constructor( 
+        public name: string,
+        public address: string
+    ) {}
+}
+
+// export class Hero extends Person {
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ) { 
+//         super( realName, 'New York'); 
+//     }
+// }
+
+export class Hero {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person,
+    ) { }
+}
+
+const tony = new Person('Tony Stark', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony Stark', tony);
+console.log(ironman);
